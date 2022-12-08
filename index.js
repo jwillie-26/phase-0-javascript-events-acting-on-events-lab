cf22a4ca4ab991bd7bfbfa83e4ab7c3d9ce21e45
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2022-12-08 09:06:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-var dodger = document.getElementById('dodger')
+var dodger = document.getElementById('dodger');
 
-dodger.style.backgroundColor = "pink"
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
-
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`
   }
 }
 
 document.addEventListener('keydown', function(e) {
-  if (e.which === 39){
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
     moveDodgerRight()
   }
-}
-)
+})
